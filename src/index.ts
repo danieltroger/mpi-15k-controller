b2fc22cfbef3666c822367f01bb3ff4c8a76bba1
Daniel Troger <daniel@natur-kultur.eu>
2024-04-16 00:10:41 +0200

    Configurable mqtt host

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ function main() {
     const configResourceValue = configResource();
     if (!configResourceValue) return;
     const [config] = configResourceValue;
-    const mqttValues = useMQTTValues();
+    const mqttValues = useMQTTValues(() => config().mqtt_host);
     const hasCredentials = createMemo(() => !!(config().shinemonitor_password && config().shinemonitor_user));
     const hasInverterDetails = createMemo(() => !!(config().inverter_sn && config().inverter_sn));
     const [prematureWorkaroundErrored, setPrematureWorkaroundErrored] = createSignal(false);
