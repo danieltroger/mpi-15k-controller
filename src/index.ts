a20ec074c597d2667c73352eda4d46525719a1b3
Daniel Troger <daniel@natur-kultur.eu>
2024-04-21 22:11:17 +0200

    update todo

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ function main() {
   // TODO: add ws messaging + frontend
   // TODO: consider how much sun is shining in when full current if-statement
   // TODO: limit discharge current as voltage gets lower and limit charge current as voltage gets higher
+  // TODO: add SOC calculation
   const owner = getOwner()!;
   const [configResource] = createResource(() => get_config_object(owner));
 
