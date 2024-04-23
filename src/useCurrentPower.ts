bd8edf8f937d77e99fbff60e16cdd5fea83549db
Daniel Troger <daniel@natur-kultur.eu>
2024-04-23 11:00:25 +0200

    More temp log

--- src/useCurrentPower.ts
+++ src/useCurrentPower.ts
@@ -66,5 +66,7 @@ export function useCurrentPower(
     }
   });
 
+  createEffect(() => console.log("local power history", JSON.parse(JSON.stringify(localPowerHistory))));
+
   return { localPowerHistory, currentPower, lastBatterySeenFullSinceProgramStart: haveSeenBatteryFullAt };
 }
