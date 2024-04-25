18b4e5f0c99b4bad4ddff1e18aa8ffc8f52d1576
Daniel Troger <daniel@natur-kultur.eu>
2024-04-25 23:24:15 +0200

    Add some untested WS messaging

---
+++ src/startWsServer.ts
@@ -0,0 +1,77 @@
+import { WebSocket, WebSocketServer, Server } from "ws";
+import { error, log, warn } from "./logging";
+import { IncomingMessage } from "http";
+import { catchify } from "@depict-ai/utilishared";
+
+const max_connection_age = 1000 * 60 * 60; //ms
+const ping_interval = 1000 * 5; // ms
+const port = 9321;
+const connections = new Set<WebSocket>();
+
+export async function startWsServer<T extends { id: string; [key: string]: any }>(
+  handle_message: (message: T) => Promise<string>
+) {
+  let wss: Server<typeof WebSocket, typeof IncomingMessage>;
+  const start_server = () => {
+    wss = new WebSocketServer({ port });
+    wss.on(
+      "error",
+      catchify(async (e: Error) => {
+        error("WS server had an error", e, "restarting it in 5s");
+        wss.close();
+        await new Promise(r => setTimeout(r, 5000));
+        start_server();
+      })
+    );
+
+    wss.on(
+      "connection",
+      catchify(async ws => {
+        connections.add(ws);
+        let last_movement = +new Date();
+        ws.on("close", () => (last_movement = 0));
+        ws.on("error", m => {
+          log("Connection had error, killing it", m);
+          last_movement = 0;
+        });
+        ws.on("pong", () => (last_movement = +new Date()));
+        ws.on(
+          "message",
+          catchify(async data => {
+            const decoded = JSON.parse(data.toString());
+            if (!decoded.id) {
+              warn("Cannot handle message", decoded, data, "because it doesn't have an id");
+              return;
+            }
+            ws.send(
+              JSON.stringify({
+                id: decoded.id,
+                status: "ack",
+              })
+            );
+            const response = await handle_message(decoded);
+            if (response) {
+              ws.send(response);
+            }
+          })
+        );
+        while (+new Date() - last_movement < max_connection_age) {
+          ws.ping(Math.random() + "");
+          await new Promise(r => setTimeout(r, ping_interval));
+        }
+        ws.close();
+        connections.delete(ws);
+      })
+    );
+
+    log("Started websocket server");
+  };
+
+  start_server();
+  const broadcast = (msg: string) => {
+    for (const connection of connections) {
+      connection.send(msg);
+    }
+  };
+  return { broadcast };
+}
---
+++ src/wsMessaging.ts
@@ -0,0 +1,73 @@
+import { Accessor, createEffect, Owner, runWithOwner, Signal, untrack } from "solid-js";
+import { Config } from "./config";
+import { startWsServer } from "./startWsServer";
+
+export async function wsMessaging({
+  config_signal: [get_config, set_config],
+  info,
+  owner,
+}: {
+  config_signal: Signal<Config>;
+  info: Accessor<Record<string, any>>;
+  owner: Owner;
+}) {
+  const exposed_signals = {
+    config: {
+      getter: get_config,
+      setter: set_config,
+      validator: value => {
+        if (typeof value !== "object") {
+          return "Can't write config, not an object: " + value;
+        }
+      },
+    },
+    info: { getter: info },
+  } as const;
+
+  const { broadcast } = await startWsServer(async (msg: { [key: string]: any }) => {
+    const { command, key, value, id } = msg;
+
+    if (command === "read" || command === "write") {
+      const specifier = exposed_signals[key as keyof typeof exposed_signals];
+      if (!specifier) {
+        return JSON.stringify({
+          id,
+          status: "not-ok",
+          message: `No signal with key: ${key}, allowed keys: ${Object.keys(exposed_signals).join(", ")}`,
+        });
+      }
+      const { getter } = specifier;
+      if (command === "read") {
+        return JSON.stringify({ id, status: "ok", value: untrack(getter) });
+      } else if (command === "write") {
+        if (!("setter" in specifier)) {
+          return JSON.stringify({
+            id,
+            status: "not-ok",
+            message: `Can't write to signal with key: ${key}, it is read-only`,
+          });
+        }
+        if ("validator" in specifier) {
+          const error = specifier.validator(value);
+          if (error) {
+            return JSON.stringify({ id, status: "not-ok", message: error });
+          }
+        }
+        specifier.setter(value);
+        return JSON.stringify({ id, status: "ok", value });
+      }
+    }
+    return JSON.stringify({
+      id,
+      status: "not-ok",
+      message: "Command not recognized: " + command,
+    });
+  });
+
+  for (const key in exposed_signals) {
+    const { getter } = exposed_signals[key as keyof typeof exposed_signals];
+    runWithOwner(owner, () =>
+      createEffect(() => broadcast(JSON.stringify({ id: Math.random() + "", type: "change", key, "value": getter() })))
+    );
+  }
+}
