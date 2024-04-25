7e70fb653bad83170a8b3945acab55a8997cdb42
Daniel Troger <daniel@natur-kultur.eu>
2024-04-26 00:09:33 +0200

    Show more data

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,18 +3,38 @@ import { createSignal, onMount, Show } from "solid-js";
 
 export default function Home() {
   const [info] = getBackendSyncedSignal("info");
+  const [mqttValues] = getBackendSyncedSignal("mqttValues");
   const [hasHydrated, setHasHydrated] = createSignal(false);
   onMount(() => setHasHydrated(true));
 
   return (
     <main>
-      <pre>
-        <code>
-          <Show when={hasHydrated() && info()} fallback={"Loading…"}>
-            {JSON.stringify(info(), null, 2)}
-          </Show>
-        </code>
-      </pre>
+      <section>
+        <h2>Links</h2>
+        <ol>
+          <li>Config editor</li>
+        </ol>
+      </section>
+      <section>
+        <h2>Info</h2>
+        <pre>
+          <code>
+            <Show when={hasHydrated() && info()} fallback={"Loading…"}>
+              {JSON.stringify(info(), null, 2)}
+            </Show>
+          </code>
+        </pre>
+      </section>
+      <section>
+        <h2>MQTT Values</h2>
+        <pre>
+          <code>
+            <Show when={hasHydrated() && mqttValues()} fallback={"Loading…"}>
+              {JSON.stringify(mqttValues(), null, 2)}
+            </Show>
+          </code>
+        </pre>
+      </section>
     </main>
   );
 }
