c5223789d810f4957414091bcb69b1e92195dd87
Daniel Troger <daniel@natur-kultur.eu>
2024-04-26 00:05:24 +0200

    We have data in the frontend

--- frontend/src/components/WebSocketProvider.tsx
+++ frontend/src/components/WebSocketProvider.tsx
@@ -1,22 +1,19 @@
 import { DepictAPIWS } from "@depict-ai/utilishared/latest";
 import { createContext, JSX, useContext } from "solid-js";
-import { getRequestEvent, isServer } from "solid-js/web";
+import { isServer } from "solid-js/web";
 
 const WebSocketContext = createContext<DepictAPIWS>();
 
 export function WebSocketProvider(props: { children?: JSX.Element }) {
-  let origin: string;
-  if (isServer) {
-    const event = getRequestEvent();
-    origin = new URL(event!.request.url).origin;
-  } else {
-    ({ origin } = location);
-  }
-  const u_o = new URL(origin);
-  u_o.protocol = u_o.protocol === "https:" ? "wss:" : "ws:";
-  u_o.port = "9321";
+  let socket: DepictAPIWS | undefined;
 
-  const socket = new DepictAPIWS(u_o.toString());
+  if (!isServer) {
+    const u_o = new URL(location.origin);
+    u_o.protocol = u_o.protocol === "https:" ? "wss:" : "ws:";
+    u_o.port = "9321";
+
+    socket = new DepictAPIWS(u_o.toString());
+  }
 
   return <WebSocketContext.Provider value={socket}>{props.children}</WebSocketContext.Provider>;
 }
