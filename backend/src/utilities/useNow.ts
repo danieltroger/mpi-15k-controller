086134956a02767513d152cb7d4c5351e7db77e5
Daniel Troger <daniel@natur-kultur.eu>
2024-05-07 09:03:09 +0200

    Maybe fix morning grid feeding bouncyness

--- backend/src/utilities/useNow.ts
+++ backend/src/utilities/useNow.ts
@@ -2,7 +2,7 @@ import { createSignal, onCleanup } from "solid-js";
 
 export function useNow() {
   const [currentTime, setCurrentTime] = createSignal(+new Date());
-  const timeInterval = setInterval(() => setCurrentTime(+new Date()), 1000);
+  const timeInterval = setInterval(() => setCurrentTime(+new Date()), 500);
   onCleanup(() => clearInterval(timeInterval));
 
   return currentTime;
