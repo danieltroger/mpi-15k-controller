6536fb965008034c3d26c1913b87ce6cb4aff95f
Daniel Troger <daniel@natur-kultur.eu>
2024-05-13 20:25:23 +0200

    Maybe fix lagging battery power?

--- backend/src/useCurrentPower.ts
+++ backend/src/useCurrentPower.ts
@@ -10,26 +10,26 @@ export function useCurrentPower(
     // The voltage is guaranteed by mpp-solar and the inverter to always update before the current
     // Both values update practically at the same time but don't get written to the mqttValues store in a batch
     // Due to the deterministic nature of the updates, we can rely on the voltage being from the same "update situation" as the current every time the current updates
-    const current = mqttValues.battery_current as undefined | { time: number; value: number };
-    if (!current) return;
-    const { value: incomingCurrent, time: amperageTimestamp } = current;
-    const voltage = untrack(
+    const voltage = mqttValues.battery_voltage as undefined | { time: number; value: number };
+    if (!voltage) return;
+    const { value: incomingVoltage, time: voltageTimestamp } = voltage;
+    const current = untrack(
       () =>
-        mqttValues.battery_voltage && {
-          time: mqttValues.battery_voltage?.time,
-          value: mqttValues?.battery_voltage?.value as number,
+        mqttValues.battery_current && {
+          time: mqttValues.battery_current?.time,
+          value: mqttValues?.battery_current?.value as number,
         }
     );
-    if (!voltage) {
+    if (!current) {
       // If we for some reason don't have a voltage, make this effect depend on it so we re-execute once we have it
-      mqttValues.battery_voltage?.value;
+      mqttValues.battery_current?.value;
       return;
     }
-    const { value: incomingVoltage, time: voltageTimestamp } = voltage;
+    const { value: incomingCurrent, time: currentTimestamp } = current;
     const voltageNow = incomingVoltage / 10;
     const currentNow = incomingCurrent / 10;
     const power = voltageNow * currentNow;
-    return { value: power, time: Math.min(amperageTimestamp, voltageTimestamp) };
+    return { value: power, time: Math.min(currentTimestamp, voltageTimestamp) };
   });
   const haveSeenBatteryFullAt = createMemo<number | undefined>(prev => {
     const voltage = mqttValues.battery_voltage?.value as undefined | number;
