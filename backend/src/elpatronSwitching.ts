53c6a746ee232b63b13eaffed646742619a205f5
Daniel Troger <daniel@natur-kultur.eu>
2024-05-26 00:10:04 +0200

    Throttle switching

--- backend/src/elpatronSwitching.ts
+++ backend/src/elpatronSwitching.ts
@@ -5,12 +5,12 @@ import { WebSocket } from "ws";
 import { DepictAPIWS, random_string } from "@depict-ai/utilishared/latest";
 import { totalSolarPower } from "./utilities/totalSolarPower";
 import { log } from "./utilities/logging";
+import { useNow } from "./utilities/useNow";
 
 // @ts-ignore
 globalThis.WebSocket = WebSocket;
 
 let socket: DepictAPIWS | undefined;
-let lastSwitched = 0;
 
 export function elpatronSwitching(
   config: Accessor<Config>,
@@ -18,28 +18,33 @@ export function elpatronSwitching(
 ) {
   const functionalityEnabled = createMemo(() => config().elpatron_switching.enabled);
   const fromSolar = createMemo(() => totalSolarPower(mqttValues));
+  const [switchingBlockedUntil, setSwitchingBlockedUntil] = createSignal(Infinity);
+  const now = useNow();
 
   createEffect(() => {
     if (!functionalityEnabled()) return;
     socket ||= new DepictAPIWS("ws://192.168.1.100:9321");
-    const elpatronShouldBeEnabled = createMemo(() => {
+    const elpatronShouldBeEnabled = createMemo<boolean | undefined>(prev => {
+      if (switchingBlockedUntil() > now()) return prev;
       const solar = fromSolar();
-      return [
-        // pass array because createResource ignores falsey values
-        solar != undefined &&
-          solar > config().elpatron_switching.min_solar_input &&
-          mqttValues.line_power_direction?.value === "Output",
-      ] as const;
+      if (solar == undefined) return;
+      setSwitchingBlockedUntil(+new Date() + 1000 * 60 * 5); // Only allow switching every 5 minutes
+      return solar > config().elpatron_switching.min_solar_input && mqttValues.line_power_direction?.value === "Output";
     });
 
-    createResource(elpatronShouldBeEnabled, async ([enable]) => {
-      const [result] = (await socket?.ensure_sent({
-        id: random_string(),
-        command: "write-gpio",
-        value: { "output": "electric_heating_element", "new_state": enable ? 0 : 1 },
-      })) as any;
+    createResource(
+      // pass array because createResource ignores falsey values
+      () => [elpatronShouldBeEnabled()] as const,
+      async ([enable]) => {
+        if (enable == undefined) return;
+        const [result] = (await socket?.ensure_sent({
+          id: random_string(),
+          command: "write-gpio",
+          value: { "output": "electric_heating_element", "new_state": enable ? 0 : 1 },
+        })) as any;
 
-      log((enable ? "Enable" : "Disable") + " elpatron result:", result);
-    });
+        log((enable ? "Enable" : "Disable") + " elpatron result:", result);
+      }
+    );
   });
 }
