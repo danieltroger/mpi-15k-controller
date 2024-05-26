b8598fd501c4e1ec95a8ea499aa38cbb46ef0f59
Daniel Troger <daniel@natur-kultur.eu>
2024-05-26 10:54:22 +0200

    More robust quick switching guard

--- backend/src/elpatronSwitching.ts
+++ backend/src/elpatronSwitching.ts
@@ -2,7 +2,7 @@ import { Config, get_config_object } from "./config";
 import { Accessor, createEffect, createMemo, createResource, createSignal } from "solid-js";
 import { useMQTTValues } from "./useMQTTValues";
 import { WebSocket } from "ws";
-import { DepictAPIWS, random_string } from "@depict-ai/utilishared/latest";
+import { DepictAPIWS, random_string, wait } from "@depict-ai/utilishared/latest";
 import { totalSolarPower } from "./utilities/totalSolarPower";
 import { log } from "./utilities/logging";
 import { useNow } from "./utilities/useNow";
@@ -11,6 +11,7 @@ import { useNow } from "./utilities/useNow";
 globalThis.WebSocket = WebSocket;
 
 let socket: DepictAPIWS | undefined;
+let lastSwitch = 0;
 
 export function elpatronSwitching(
   config: Accessor<Config>,
@@ -18,26 +19,30 @@ export function elpatronSwitching(
 ) {
   const functionalityEnabled = createMemo(() => config().elpatron_switching.enabled);
   const fromSolar = createMemo(() => totalSolarPower(mqttValues));
-  const [switchingBlockedUntil, setSwitchingBlockedUntil] = createSignal(0);
-  const now = useNow();
   const getPowerDirection = () => mqttValues.line_power_direction?.value;
 
   createEffect(() => {
     if (!functionalityEnabled()) return;
     socket ||= new DepictAPIWS("ws://192.168.1.100:9321");
     const elpatronShouldBeEnabled = createMemo<boolean | undefined>(prev => {
-      if (switchingBlockedUntil() > now()) return prev;
       const solar = fromSolar();
       const powerDirection = getPowerDirection();
       if (solar == undefined || powerDirection == undefined) return;
-      setSwitchingBlockedUntil(+new Date() + 1000 * 60 * 5); // Only allow switching every 5 minutes
       return solar > config().elpatron_switching.min_solar_input && powerDirection === "Output";
     });
 
     createResource(
       // pass array because createResource ignores falsey values
       () => [elpatronShouldBeEnabled()] as const,
-      async ([enable]) => {
+      async () => {
+        // Always get latest value
+        const timeSinceLastSwitch = +new Date() - lastSwitch;
+        const maxSwitchEvery = 1000 * 60 * 5;
+        if (timeSinceLastSwitch < maxSwitchEvery) {
+          log("Recently switched elpatron, waiting", timeSinceLastSwitch, "ms before switching again");
+          await wait(maxSwitchEvery - timeSinceLastSwitch);
+        }
+        const enable = elpatronShouldBeEnabled();
         if (enable == undefined) return;
         const [result] = (await socket?.ensure_sent({
           id: random_string(),
