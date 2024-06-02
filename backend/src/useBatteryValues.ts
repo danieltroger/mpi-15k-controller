332a87433b8f28f9e18538358da03934593a4743
Daniel Troger <daniel@natur-kultur.eu>
2024-06-02 19:04:28 +0200

    Wait for data for initial calculations?

--- backend/src/useBatteryValues.ts
+++ backend/src/useBatteryValues.ts
@@ -118,10 +118,12 @@ function iterativelyFindSocParameters({
   const [toggle, setToggle] = createSignal(false);
   const startParasiticConsumption = createMemo(() => config().soc_calculations.parasitic_consumption_from);
   const endParasiticConsumption = createMemo(() => config().soc_calculations.parasitic_consumption_to);
+  const hasData = createMemo(() => totalLastFull() !== undefined && totalLastEmpty() !== undefined);
   // Calculate SOC stuff once an hour
   setInterval(() => setToggle(prev => !prev), 1000 * 60 * 60);
 
   createEffect(() => {
+    if (!hasData()) return;
     toggle();
     const totalStartCapacity = startCapacityWh();
     const totalEndCapacity = endCapacityWh();
