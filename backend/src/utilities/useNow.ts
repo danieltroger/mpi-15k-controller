3c9e7d6fbe5221d8f5c2d4646824cb57694c42cc
Daniel Troger <daniel@natur-kultur.eu>
2024-06-02 15:35:55 +0200

    Use less cpu

--- backend/src/utilities/useNow.ts
+++ backend/src/utilities/useNow.ts
@@ -2,7 +2,7 @@ import { createSignal, onCleanup } from "solid-js";
 
 export function useNow() {
   const [currentTime, setCurrentTime] = createSignal(+new Date());
-  const timeInterval = setInterval(() => setCurrentTime(+new Date()), 500);
+  const timeInterval = setInterval(() => setCurrentTime(+new Date()), 1000);
   onCleanup(() => clearInterval(timeInterval));
 
   return currentTime;
