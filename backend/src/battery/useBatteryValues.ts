0630dab1485ed939ac54846a97fe563b4e53cd7e
Daniel Troger <daniel@natur-kultur.eu>
2024-06-10 00:20:18 +0200

    We got some telling output

--- backend/src/battery/useBatteryValues.ts
+++ backend/src/battery/useBatteryValues.ts
@@ -1,13 +1,12 @@
 import { useCurrentPower } from "./useCurrentPower";
 import { useNow } from "../utilities/useNow";
 import { useDatabasePower } from "./useDatabasePower";
-import { createMemo, createSignal, Resource, untrack } from "solid-js";
+import { createEffect, createMemo, Resource } from "solid-js";
 import { useMQTTValues } from "../useMQTTValues";
 import { get_config_object } from "../config";
-import { batteryCalculationsDependingOnUnknowns } from "./batteryCalculationsDependingOnUnknowns";
 import { AsyncMqttClient } from "async-mqtt";
-import { iterativelyFindSocParameters } from "./iterativelyFindSocParameters";
-import { reportSOCToMqtt } from "./reportSOCToMqtt";
+import { appendFile } from "fs/promises";
+import { error } from "../utilities/logging";
 
 export function useBatteryValues(
   mqttValues: ReturnType<typeof useMQTTValues>["mqttValues"],
@@ -22,82 +21,91 @@ export function useBatteryValues(
     lastBatterySeenEmptySinceProgramStart,
   } = useCurrentPower(mqttValues, configSignal);
   const now = useNow();
-  const { databasePowerValues, batteryWasLastFullAtAccordingToDatabase, batteryWasLastEmptyAtAccordingToDatabase } =
-    useDatabasePower(configSignal);
+  const {
+    databasePowerValues,
+    batteryWasLastFullAtAccordingToDatabase,
+    batteryWasLastEmptyAtAccordingToDatabase,
+    whenWereWeEmpty,
+    whenWereWeFullCirca,
+  } = useDatabasePower(configSignal);
 
-  const [totalLastFull, totalLastEmpty] = [
-    [batteryWasLastFullAtAccordingToDatabase, lastBatterySeenFullSinceProgramStart],
-    [batteryWasLastEmptyAtAccordingToDatabase, lastBatterySeenEmptySinceProgramStart],
-  ].map(([db, local]) =>
-    createMemo(() => {
-      const lastSinceStart = local();
-      const lastAccordingToDatabase = db();
-      if (!lastSinceStart && !lastAccordingToDatabase) return;
-      if (!lastSinceStart) return lastAccordingToDatabase;
-      if (!lastAccordingToDatabase) return lastSinceStart;
-      return Math.max(lastSinceStart, lastAccordingToDatabase);
-    })
-  );
   const assumedParasiticConsumption = createMemo(() => config().soc_calculations.current_state.parasitic_consumption);
   const assumedCapacity = createMemo(() => config().soc_calculations.current_state.capacity);
 
-  const {
-    energyAddedSinceEmpty,
-    energyChargedSinceEmpty,
-    energyDischargedSinceEmpty,
-    energyDischargedSinceFull,
-    energyRemovedSinceFull,
-    energyChargedSinceFull,
-    socSinceFull,
-    socSinceEmpty,
-  } = batteryCalculationsDependingOnUnknowns({
-    now,
-    localPowerHistory,
-    databasePowerValues,
-    totalLastFull,
-    totalLastEmpty,
-    subtractFromPower: assumedParasiticConsumption,
-    assumedCapacity,
+  // Capacity values assuming no parasitic consumption
+  const uncorrectedCapacity = createMemo(() => {
+    const powerValues = databasePowerValues();
+    const whenEmpty = whenWereWeEmpty();
+    const whenFull = whenWereWeFullCirca();
+    if (!powerValues.length || !whenEmpty?.length || !whenFull?.length) return;
+
+    const sortedWhenEmpty = whenEmpty.sort((a, b) => a.time - b.time).reverse();
+    const sortedWhenFull = whenFull.sort((a, b) => a.time - b.time).reverse();
+
+    const output: string[] = [];
+    let energyChargedSinceFull = 0;
+    let energyDischargedSinceFull = 0;
+    let energyChargedSinceEmpty = 0;
+    let energyDischargedSinceEmpty = 0;
+    let nextResetEmpty = sortedWhenEmpty.pop();
+    let nextResetFull = sortedWhenFull.pop();
+
+    for (let i = 0; i < powerValues.length; i++) {
+      const power = powerValues[i];
+      const nextPower = powerValues[i + 1];
+      if (!nextPower) break;
+      if (nextResetEmpty && power.time > nextResetEmpty.time) {
+        energyChargedSinceEmpty = 0;
+        energyDischargedSinceEmpty = 0;
+        nextResetEmpty = sortedWhenEmpty.pop();
+      }
+      if (nextResetFull && power.time > nextResetFull.time) {
+        energyChargedSinceFull = 0;
+        energyDischargedSinceFull = 0;
+        nextResetFull = sortedWhenFull.pop();
+      }
+      const correctedPowerValue = power.value;
+      const timeDiff = nextPower.time - power.time;
+      const energy = (correctedPowerValue * timeDiff) / 1000 / 60 / 60;
+      if (correctedPowerValue > 0) {
+        energyChargedSinceFull += energy;
+        energyChargedSinceEmpty += energy;
+      } else if (correctedPowerValue < 0) {
+        energyDischargedSinceFull += energy;
+        energyDischargedSinceEmpty += energy;
+      }
+      const energyRemovedSinceFull = Math.abs(energyDischargedSinceFull) - Math.abs(energyChargedSinceFull);
+      const energyAddedSinceEmpty = Math.abs(energyChargedSinceEmpty) - Math.abs(energyDischargedSinceEmpty);
+      output.push(
+        `soc_playground_1 capacity=${energyRemovedSinceFull + energyAddedSinceEmpty} ${Math.round(power.time / 1000)}`,
+        `soc_playground_1 energyDischargedSinceFull=${energyDischargedSinceFull} ${Math.round(power.time / 1000)}`,
+        `soc_playground_1 energyChargedSinceFull=${energyChargedSinceFull} ${Math.round(power.time / 1000)}`,
+        `soc_playground_1 energyChargedSinceEmpty=${energyChargedSinceEmpty} ${Math.round(power.time / 1000)}`,
+        `soc_playground_1 energyDischargedSinceEmpty=${energyDischargedSinceEmpty} ${Math.round(power.time / 1000)}`,
+        `soc_playground_1 energyRemovedSinceFull=${energyRemovedSinceFull} ${Math.round(power.time / 1000)}`,
+        `soc_playground_1 energyAddedSinceEmpty=${energyAddedSinceEmpty} ${Math.round(power.time / 1000)}`
+      );
+    }
+
+    return output;
   });
 
-  // iterativelyFindSocParameters({
-  //   totalLastEmpty,
-  //   totalLastFull,
-  //   now,
-  //   localPowerHistory,
-  //   databasePowerValues,
-  //   configSignal,
-  // });
+  createEffect(() => {
+    const result = uncorrectedCapacity();
+    if (!result?.length) return;
 
-  const averageSOC = createMemo(() => {
-    const sinceFull = socSinceFull();
-    const sinceEmpty = socSinceEmpty();
-    const fullInvalid = isNaN(sinceFull!) || Math.abs(sinceFull!) === Infinity;
-    const emptyInvalid = isNaN(sinceEmpty!) || Math.abs(sinceEmpty!) === Infinity;
-    if (fullInvalid || emptyInvalid) return;
-    return (sinceFull! + sinceEmpty!) / 2;
-  });
-
-  reportSOCToMqtt({
-    mqttClient,
-    config,
-    averageSOC,
-    socSinceEmpty,
-    socSinceFull,
+    appendFile(
+      "simulation-output-" + new Date().toISOString() + ".txt",
+      `# DML
+# CONTEXT-DATABASE: mppsolar
+# CONTEXT-RETENTION-POLICY: autogen
+` + result.join("\n"),
+      "utf8"
+    ).catch(e => error("Failed to save output", e));
   });
 
   return {
-    energyChargedSinceFull,
-    energyChargedSinceEmpty,
-    energyDischargedSinceEmpty,
-    energyDischargedSinceFull,
     currentPower,
-    totalLastEmpty,
-    totalLastFull,
-    energyRemovedSinceFull,
-    energyAddedSinceEmpty,
-    socSinceEmpty,
-    socSinceFull,
     assumedCapacity,
     assumedParasiticConsumption,
   };
