8cd4d47130071ca021217218fedf73d4c82163ac
Daniel Troger <daniel@natur-kultur.eu>
2024-08-25 21:41:31 +0200

    Don't update now as often to save some CPU

--- backend/src/utilities/useNow.ts
+++ backend/src/utilities/useNow.ts
@@ -2,7 +2,7 @@ import { createSignal, onCleanup } from "solid-js";
 
 export function useNow() {
   const [currentTime, setCurrentTime] = createSignal(+new Date());
-  const timeInterval = setInterval(() => setCurrentTime(+new Date()), 1000);
+  const timeInterval = setInterval(() => setCurrentTime(+new Date()), 2000);
   onCleanup(() => clearInterval(timeInterval));
 
   return currentTime;
