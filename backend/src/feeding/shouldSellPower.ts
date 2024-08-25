351021d372e954bf370f476835a07f1dae7d32fd
Daniel Troger <daniel@natur-kultur.eu>
2024-08-25 22:00:31 +0200

    debug

--- backend/src/feeding/shouldSellPower.ts
+++ backend/src/feeding/shouldSellPower.ts
@@ -18,19 +18,25 @@ export function shouldSellPower(config: Accessor<Config>, averageSOC: Accessor<n
 
           // If already in the timeslot, set feeding directly
           if (start <= now && now <= end) {
+            console.log("In feeding timeslot, starting directly");
             setWantedOutput(() => () => schedule().power_watts);
           } else if (start > now) {
             // If schedule item starts in the future, set timeout for both start and end
+            console.log("scheduling start timer in", start - now, "and end timer in", end - now);
             const startTimeout = setTimeout(() => setWantedOutput(() => () => schedule().power_watts), start - now);
             const endTimeout = setTimeout(() => setWantedOutput(() => () => 0), end - now);
 
             onCleanup(() => {
+              console.log("deleting timeout");
               clearTimeout(startTimeout);
               clearTimeout(endTimeout);
             });
           }
 
-          onCleanup(() => setWantedOutput(() => () => 0));
+          onCleanup(() => {
+            console.log("cleanup, setting to 0 ");
+            setWantedOutput(() => () => 0);
+          });
         });
         return wantedOutput;
       }
