6d8449c0d40d9c1b13eb13907901f3b7de2bcf0a
Daniel Troger <daniel@natur-kultur.eu>
2024-08-25 22:23:06 +0200

    Holy guacemoly it works

--- backend/src/feeding/shouldSellPower.ts
+++ backend/src/feeding/shouldSellPower.ts
@@ -17,35 +17,22 @@ export function shouldSellPower(config: Accessor<Config>, averageSOC: Accessor<n
           const end = memoizedEnd();
           const setEndTimeout = () => {
             const endTimeout = setTimeout(() => setWantedOutput(() => () => 0), end - now);
-            onCleanup(() => {
-              console.log("deleting end timeout");
-              clearTimeout(endTimeout);
-            });
+            onCleanup(() => clearTimeout(endTimeout));
           };
-
-          console.log("effect run for now", now, "start", start, "end", end);
 
           // If already in the timeslot, set feeding directly
           if (start <= now && now <= end) {
-            console.log("In feeding timeslot, starting directly");
             setWantedOutput(() => () => schedule().power_watts);
             setEndTimeout();
           } else if (start > now) {
             // If schedule item starts in the future, set timeout for both start and end
-            console.log("scheduling start timer in", start - now, "and end timer in", end - now);
             const startTimeout = setTimeout(() => setWantedOutput(() => () => schedule().power_watts), start - now);
             setEndTimeout();
 
-            onCleanup(() => {
-              console.log("deleting start timeout");
-              clearTimeout(startTimeout);
-            });
+            onCleanup(() => clearTimeout(startTimeout));
           }
 
-          onCleanup(() => {
-            console.log("cleanup, setting to 0 ");
-            setWantedOutput(() => () => 0);
-          });
+          onCleanup(() => setWantedOutput(() => () => 0));
         });
         return wantedOutput;
       }
@@ -67,7 +54,11 @@ export function shouldSellPower(config: Accessor<Config>, averageSOC: Accessor<n
     return 0;
   });
 
-  createEffect(() => log("Starting to feed in " + exportAmountForSelling() + "watts due to scheduled power selling"));
+  createEffect(
+    () =>
+      exportAmountForSelling() &&
+      log("Feed in due to scheduled power selling wants to feed in " + exportAmountForSelling() + "watts")
+  );
 
   return { exportAmountForSelling };
 }
