f7b48f401e9c5993d4eb287cb9b1512eeb88cc31
Daniel Troger <daniel@natur-kultur.eu>
2024-10-05 09:27:50 +0200

    Increase anti feed in pushing and don't do it when off-grid

--- backend/src/feeding/useOutputPowerSuddenlyRose.ts
+++ backend/src/feeding/useOutputPowerSuddenlyRose.ts
@@ -1,25 +1,31 @@
 import { Accessor, createEffect, createSignal } from "solid-js";
 import { Config } from "../config";
+import { useMQTTValues } from "../useMQTTValues";
 
 /**
  * When ac output power suddenly rises, the inverter will pull power from the grid even though it shouldn't. Increment the amount that we feed into the grid already to counteract the inverters own consumption from the grid by even more to ensure the peak is covered by energy from the battery instead.
  */
-export function useOutputPowerSuddenlyRose(acOutputPower: Accessor<undefined | number>, config: Accessor<Config>) {
-  let resetTimeout: NodeJS.Timeout | undefined;
-  const [outputPowerSuddenlyRose, setOutputPowerSuddenlyRose] = createSignal(false);
+export function useOutputPowerSuddenlyRose(
+  acOutputPower: Accessor<undefined | number>,
+  config: Accessor<Config>,
+  mqttValues: ReturnType<typeof useMQTTValues>["mqttValues"]
+) {
+  const getPowerDirection = () => mqttValues.line_power_direction?.value;
+
+  const [incrementForAntiPeak, setIncrementForAntiPeak] = createSignal(0);
   createEffect<number | undefined>(prev => {
     const latestValue = acOutputPower();
-    if (latestValue == undefined || prev == undefined) {
+    if (latestValue == undefined || prev == undefined || getPowerDirection() === "Idle") {
       return latestValue;
     }
-    const { peak_min_change, peak_increment_duration } = config().feed_from_battery_when_no_solar;
+    const { peak_min_change, peak_increment_duration, increment_with_on_peak } =
+      config().feed_from_battery_when_no_solar;
     if (latestValue - prev > peak_min_change) {
-      setOutputPowerSuddenlyRose(true);
-      clearTimeout(resetTimeout);
-      resetTimeout = setTimeout(() => setOutputPowerSuddenlyRose(false), peak_increment_duration * 1000);
+      setIncrementForAntiPeak(prev => prev + increment_with_on_peak);
+      setTimeout(() => setIncrementForAntiPeak(prev => prev - increment_with_on_peak), peak_increment_duration * 1000);
     }
     return latestValue;
   });
 
-  return outputPowerSuddenlyRose;
+  return incrementForAntiPeak;
 }
