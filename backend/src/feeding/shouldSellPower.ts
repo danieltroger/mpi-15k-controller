170b4d7617c9314788281a2c5ddce7803f70fba6
Daniel Troger <daniel@natur-kultur.eu>
2024-10-10 20:32:23 +0200

    Lil refactor before scheduled power buying

--- backend/src/feeding/shouldSellPower.ts
+++ backend/src/feeding/shouldSellPower.ts
@@ -1,12 +1,7 @@
-import { Accessor, createEffect, createMemo, createSignal, indexArray, mapArray, onCleanup } from "solid-js";
+import { Accessor, createEffect, createMemo, createSignal, mapArray } from "solid-js";
 import { Config } from "../config";
 import { log } from "../utilities/logging";
-import { catchify } from "@depict-ai/utilishared/latest";
-
-const timeoutBatches = new Map<
-  number,
-  { first: Set<VoidFunction>; last: Set<VoidFunction>; timeout: ReturnType<typeof setTimeout> }
->();
+import { batchedRunAtFutureTimeWithPriority } from "../utilities/batchedRunAtFutureTimeWithPriority";
 
 export function shouldSellPower(config: Accessor<Config>, averageSOC: Accessor<number | undefined>) {
   const scheduleOutput = createMemo(
@@ -74,51 +69,4 @@ export function shouldSellPower(config: Accessor<Config>, averageSOC: Accessor<n
   createEffect(() => log("Feed in due to scheduled power selling wants to feed in", exportAmountForSelling(), "watts"));
 
   return { exportAmountForSelling };
-}
-
-/**
- * Function that runs a function at a future time with the option to prioritize it over other, non-prioritized functions scheduled at the same time.
- * The idea is that we want to always start feeding before we stop feeding, to avoid momentarily feeding 0 which re-starts the slow rampup of the inverter.
- */
-function batchedRunAtFutureTimeWithPriority(fn: VoidFunction, when: number, prioritised: boolean) {
-  const existing = timeoutBatches.get(when);
-  const { first, last } = existing || { first: new Set<VoidFunction>(), last: new Set<VoidFunction>() };
-  if (!existing) {
-    const now = +new Date();
-    const timeUntil = when - now;
-    const timeout = setTimeout(
-      catchify(() => {
-        for (const fn of first) {
-          fn();
-        }
-        for (const fn of last) {
-          fn();
-        }
-        timeoutBatches.delete(when);
-      }),
-      timeUntil
-    );
-    timeoutBatches.set(when, { first, last, timeout });
-  }
-
-  const unsetTimeoutWhenEmpty = () => {
-    if (first.size !== 0 || last.size !== 0) return;
-    const timeoutToClear = timeoutBatches.get(when)?.timeout;
-    if (timeoutToClear == undefined) return;
-    clearTimeout(timeoutToClear);
-    timeoutBatches.delete(when);
-  };
-  if (prioritised) {
-    first.add(fn);
-    onCleanup(() => {
-      first.delete(fn);
-      unsetTimeoutWhenEmpty();
-    });
-  } else {
-    last.add(fn);
-    onCleanup(() => {
-      last.delete(fn);
-      unsetTimeoutWhenEmpty();
-    });
-  }
 }
