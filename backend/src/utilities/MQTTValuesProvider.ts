eb0be72af607b4c3dff19850c974acd01e5769ff
Daniel Troger <daniel@natur-kultur.eu>
2024-11-24 01:46:33 +0100

    Forgot to add file

---
+++ backend/src/utilities/MQTTValuesProvider.ts
@@ -0,0 +1,21 @@
+import { Accessor, createContext, JSX, useContext } from "solid-js";
+import { useMQTTValues } from "../useMQTTValues";
+
+const MqttValuesContext = createContext<ReturnType<typeof useMQTTValues>>();
+
+export function MQTTValuesProvider(props: { children?: JSX.Element; mqttHost: Accessor<string> }) {
+  return MqttValuesContext.Provider({
+    value: useMQTTValues(() => props.mqttHost()),
+    get children() {
+      return props.children;
+    },
+  });
+}
+
+export function useFromMqttProvider() {
+  const contextValue = useContext(MqttValuesContext);
+  if (!contextValue) {
+    throw new Error("useFromMqttProvider must be used within a MQTTValuesProvider");
+  }
+  return contextValue;
+}
