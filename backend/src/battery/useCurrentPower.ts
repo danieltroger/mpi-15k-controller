7e0a03356a25c195c027d43d0dd0c9e79ebd54b0
Daniel Troger <daniel@natur-kultur.eu>
2024-12-26 11:08:16 +0100

    Too complicated aborted refactoring

--- backend/src/battery/useCurrentPower.ts
+++ backend/src/battery/useCurrentPower.ts
@@ -1,26 +1,32 @@
 import { get_config_object } from "../config";
 import { createMemo, untrack } from "solid-js";
-import { useFromMqttProvider } from "../mqttValues/MQTTValuesProvider";
+import {
+  reactiveBatteryCurrent,
+  reactiveBatteryCurrentTime,
+  reactiveBatteryVoltage,
+  reactiveBatteryVoltageTime,
+  useFromMqttProvider,
+} from "../mqttValues/MQTTValuesProvider";
 
 export function useCurrentPower([config]: Awaited<ReturnType<typeof get_config_object>>) {
-  const { mqttValues } = useFromMqttProvider();
   const currentPower = createMemo(() => {
     // The voltage is guaranteed by mpp-solar and the inverter to always update before the current
     // Both values update practically at the same time but don't get written to the mqttValues store in a batch
     // Due to the deterministic nature of the updates, we can rely on the voltage being from the same "update situation" as the current every time the current updates
-    const current = mqttValues.battery_current as undefined | { time: number; value: number };
-    if (!current) return;
-    const { value: incomingCurrent, time: amperageTimestamp } = current;
-    const voltage = untrack(
-      () =>
-        mqttValues.battery_voltage && {
-          time: mqttValues.battery_voltage?.time,
-          value: mqttValues?.battery_voltage?.value as number,
-        }
+    const incomingCurrent = reactiveBatteryCurrent();
+    const amperageTimestamp = reactiveBatteryCurrentTime();
+    if (!incomingCurrent || !amperageTimestamp) return;
+    const voltage = untrack(() =>
+      reactiveBatteryVoltage()
+        ? {
+            time: reactiveBatteryVoltageTime(),
+            value: reactiveBatteryVoltage(),
+          }
+        : undefined
     );
     if (!voltage) {
       // If we for some reason don't have a voltage, make this effect depend on it so we re-execute once we have it
-      mqttValues.battery_voltage?.value;
+      reactiveBatteryVoltageTime();
       return;
     }
     const { value: incomingVoltage, time: voltageTimestamp } = voltage;
