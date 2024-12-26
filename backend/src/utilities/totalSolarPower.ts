2e8bf101698a1a7c58907287396fec128b887681
Daniel Troger <daniel@natur-kultur.eu>
2024-12-26 10:50:10 +0100

    Remove redundant typecasts

--- backend/src/utilities/totalSolarPower.ts
+++ backend/src/utilities/totalSolarPower.ts
@@ -1,8 +1,8 @@
 import { useMQTTValues } from "../mqttValues/useMQTTValues";
 
 export const totalSolarPower = (mqttValues: ReturnType<typeof useMQTTValues>["mqttValues"]) => {
-  const array1 = mqttValues?.["solar_input_power_1"]?.value as number | undefined;
-  const array2 = mqttValues?.["solar_input_power_2"]?.value as number | undefined;
+  const array1 = mqttValues?.["solar_input_power_1"]?.value;
+  const array2 = mqttValues?.["solar_input_power_2"]?.value;
   if (array1 == undefined && array2 == undefined) return undefined;
   return (array1 || 0) + (array2 || 0);
 };
