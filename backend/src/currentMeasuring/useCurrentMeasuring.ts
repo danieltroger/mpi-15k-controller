eec8e5516c9c26c38e84f1171a3013fa887ceb42
Daniel Troger <daniel@natur-kultur.eu>
2025-01-31 21:58:12 +0100

    Some backend stuff

--- backend/src/currentMeasuring/useCurrentMeasuring.ts
+++ backend/src/currentMeasuring/useCurrentMeasuring.ts
@@ -1,17 +1,44 @@
 import adc from "@iiot2k/ads1115";
-import { createSignal, onCleanup, Setter } from "solid-js";
+import { Accessor, createEffect, createSignal, onCleanup, Setter, untrack } from "solid-js";
 import { error } from "../utilities/logging";
+import { useFromMqttProvider } from "../mqttValues/MQTTValuesProvider";
+import { Config } from "../config";
 
 const PORT = 1; // i2c-1
 const RAWDATA = false;
 
-export function useCurrentMeasuring() {
+export function useCurrentMeasuring(config: Accessor<Config>) {
   const [voltageSagMillivolts, setVoltageSagMillivolts] = createSignal<number | undefined>(undefined);
+  const [toggleMqttSend, setToggleMqttSend] = createSignal(false);
   let cleanedUp = false;
 
   onCleanup(() => (cleanedUp = true));
 
   makeReading(setVoltageSagMillivolts, () => cleanedUp);
+
+  createEffect(() => {
+    const value = voltageSagMillivolts();
+    if (value == undefined) return;
+    toggleMqttSend();
+    reportToMqtt(value, config);
+
+    // Always send a datapoint at least every 120 seconds so that grafana doesn't create gradients over long time periods when the value has stayed the same for long
+    const grafanaReportTimeout = setTimeout(() => setToggleMqttSend(prev => !prev), 120_000);
+    onCleanup(() => clearTimeout(grafanaReportTimeout));
+  });
+
+  return { voltageSagMillivolts };
+}
+
+function reportToMqtt(value: number, config: Accessor<Config>) {
+  const { mqttClient } = useFromMqttProvider();
+  const client = mqttClient();
+  if (!client) return;
+  const table = untrack(() => config().current_measuring.table);
+  const influx_entry = `${table} raw_voltage_mv=${value}`;
+  if (client.connected) {
+    client.publish(table, influx_entry).catch(() => {});
+  }
 }
 
 function makeReading(setValue: Setter<number | undefined>, getWasCleanedUp: () => boolean) {
@@ -27,6 +54,9 @@ function makeReading(setValue: Setter<number | undefined>, getWasCleanedUp: () =
         error("Failed reading amperemeter ADC", adc.error_text());
       } else {
         setValue(data);
+        if (!getWasCleanedUp()) {
+          makeReading(setValue, getWasCleanedUp);
+        }
       }
     }
   );
