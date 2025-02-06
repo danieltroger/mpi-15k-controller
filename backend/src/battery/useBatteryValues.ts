f1a2260b54ec0ae07ad62b83e2a0814621727112
Daniel <daniel@natur-kultur.eu>
2025-02-06 12:19:02 +0100

    Revert "Use live power measurements from ADS1115"

--- backend/src/battery/useBatteryValues.ts
+++ backend/src/battery/useBatteryValues.ts
@@ -1,19 +1,16 @@
-import { useLastFullAndEmpty } from "./useLastFullAndEmpty";
+import { useCurrentPower } from "./useCurrentPower";
 import { useDatabasePower } from "./useDatabasePower";
-import { Accessor, catchError, createEffect, createMemo, createSignal } from "solid-js";
+import { catchError, createEffect, createMemo, createSignal } from "solid-js";
 import { get_config_object } from "../config";
 import { batteryCalculationsDependingOnUnknowns } from "./batteryCalculationsDependingOnUnknowns";
 import { iterativelyFindSocParameters } from "./iterativelyFindSocParameters";
 import { reportSOCToMqtt } from "./reportSOCToMqtt";
 import { error } from "../utilities/logging";
 
-export function useBatteryValues(
-  configSignal: Awaited<ReturnType<typeof get_config_object>>,
-  currentPower: Accessor<{ value: number; time: number } | undefined>
-) {
+export function useBatteryValues(configSignal: Awaited<ReturnType<typeof get_config_object>>) {
   const [config] = configSignal;
-  const { lastBatterySeenFullSinceProgramStart, lastBatterySeenEmptySinceProgramStart } =
-    useLastFullAndEmpty(configSignal);
+  const { currentPower, lastBatterySeenFullSinceProgramStart, lastBatterySeenEmptySinceProgramStart } =
+    useCurrentPower(configSignal);
   const { databasePowerValues, batteryWasLastFullAtAccordingToDatabase, batteryWasLastEmptyAtAccordingToDatabase } =
     useDatabasePower(configSignal);
 
