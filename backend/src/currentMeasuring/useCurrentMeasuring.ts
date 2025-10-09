5607f7508170fd6911aa46bbf49025f681edfa4d
Daniel Troger <daniel@natur-kultur.eu>
2025-10-09 22:02:03 +0200

    Activate the redundant current measuring sensor

--- backend/src/currentMeasuring/useCurrentMeasuring.ts
+++ backend/src/currentMeasuring/useCurrentMeasuring.ts
@@ -8,7 +8,7 @@ import { Config } from "../config.types";
 
 const PORT = 1; // i2c-1
 
-export function useCurrentMeasuring(config: Accessor<Config>) {
+export function useCurrentMeasuring(config: Accessor<Config>, sensor2: boolean) {
   const [rawMeasurement, setRawMeasurement] = createSignal<{ value: number; time: number } | undefined>(undefined); // in millivolts
   let cleanedUp = false;
   onCleanup(() => (cleanedUp = true));
@@ -16,6 +16,7 @@ export function useCurrentMeasuring(config: Accessor<Config>) {
     setValue: setRawMeasurement,
     getWasCleanedUp: () => cleanedUp,
     getRate: () => untrack(() => config().current_measuring.rate_constant),
+    sensor2,
   });
 
   const zeroCurrentMillivolts = createMemo(() => config().current_measuring.zero_current_millivolts);
@@ -31,9 +32,9 @@ export function useCurrentMeasuring(config: Accessor<Config>) {
   });
   const averagedPower = useAverageCurrent({ rawMeasurement: calculatedPowerFromAmpMeter, config });
 
-  createEffect(() => reportToMqtt(rawMeasurement()?.value, config, "raw_voltage_mv"));
-  createEffect(() => reportToMqtt(averagedMeasurement(), config, "voltage_mv_averaged"));
-  createEffect(() => reportToMqtt(averagedPower(), config, "calculated_power"));
+  createEffect(() => reportToMqtt(rawMeasurement()?.value, config, `raw_voltage_mv${sensor2 ? "_2" : ""}`));
+  createEffect(() => reportToMqtt(averagedMeasurement(), config, `voltage_mv_averaged${sensor2 ? "_2" : ""}`));
+  createEffect(() => reportToMqtt(averagedPower(), config, `calculated_power${sensor2 ? "_2" : ""}`));
 
   return {
     voltageSagMillivoltsRaw: rawMeasurement,
@@ -58,26 +59,36 @@ function makeReading({
   setValue,
   getWasCleanedUp,
   getRate,
+  sensor2,
 }: {
   setValue: Setter<{ value: number; time: number } | undefined>;
   getWasCleanedUp: () => boolean;
   getRate: () => number;
+  sensor2: boolean;
 }) {
-  adc.read(PORT, adc.ADR_48, adc.MUX_I0_GND, adc.GAIN_4096, getRate(), true, async function (data) {
-    if (data === undefined) {
-      error("Failed reading amperemeter ADC:", adc.error_text());
-    } else {
-      // The conversion formula from raw data to millivolts is:
-      // mV = raw_value × (full_scale_range_mV / 32768)
-      // Example calculation:  mV = raw_value × (4096 / 32768) = raw_value / 8 ✓
-      setValue({ value: data / 8, time: +new Date() });
-    }
-    if (!getWasCleanedUp()) {
+  adc.read(
+    PORT,
+    adc.ADR_48,
+    sensor2 ? adc.MUX_I1_GND : adc.MUX_I0_GND,
+    adc.GAIN_4096,
+    getRate(),
+    true,
+    async function (data) {
       if (data === undefined) {
-        // If failed, wait a bit before making the next reading
-        await new Promise(r => setTimeout(r, 1000));
+        error("Failed reading amperemeter ADC:", adc.error_text());
+      } else {
+        // The conversion formula from raw data to millivolts is:
+        // mV = raw_value × (full_scale_range_mV / 32768)
+        // Example calculation:  mV = raw_value × (4096 / 32768) = raw_value / 8 ✓
+        setValue({ value: data / 8, time: +new Date() });
       }
-      makeReading({ setValue: setValue, getWasCleanedUp: getWasCleanedUp, getRate });
+      if (!getWasCleanedUp()) {
+        if (data === undefined) {
+          // If failed, wait a bit before making the next reading
+          await new Promise(r => setTimeout(r, 1000));
+        }
+        makeReading({ setValue: setValue, getWasCleanedUp: getWasCleanedUp, getRate, sensor2 });
+      }
     }
-  });
+  );
 }
